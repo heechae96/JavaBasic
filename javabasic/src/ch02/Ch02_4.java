43078bb00c7f166038f97f6960bd72a26b7463b2
heechae96 <shinheechae@naver.com>
2022-02-28 15:43:07 +0900

    변수의 타

---
+++ javabasic/src/ch02/Ch02_4.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class Ch02_4 {
+
+	public static void main(String[] args) {
+		
+		// 값의 타입
+		// 기본형 8가
+		// 문자 char
+		// 정수 byte short int long
+		// 실수 float double
+		// 논리 boolean (true , false)
+
+	}
+
+}
