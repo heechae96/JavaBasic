402003c66adc0ec6e648249bb3a106e123c68d75
heechae96 <shinheechae@naver.com>
2022-02-28 11:53:15 +0900

    화면에 글자출력

---
+++ javabasic/src/ch02/Ch02_1_2.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Ch02_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.out.println("test");
+	}
+
+}
