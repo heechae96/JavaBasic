1f38059370fe721417da743bb24eee8624c51a98
heechae96 <shinheechae@naver.com>
2022-02-28 15:36:23 +0900

    변수란? 변수의 선언과 저장

---
+++ javabasic/src/ch02/Ch02_3.java
@@ -0,0 +1,21 @@
+package ch02;
+
+public class Ch02_3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("6+3");
+		System.out.println(6+3);
+		
+		// 변수 생성 후 사칙연산 
+		int x = 4 , y = 2;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		
+		
+
+	}
+
+}
