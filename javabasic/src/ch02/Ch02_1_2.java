eab8738e5dcf3ef22c1b34794f80cf4bac6673f9
heechae96 <shinheechae@naver.com>
2022-02-28 12:04:12 +0900

    화면에 글자 출력 / 사칙연산

--- javabasic/src/ch02/Ch02_1_2.java
+++ javabasic/src/ch02/Ch02_1_2.java
@@ -3,9 +3,30 @@ package ch02;
 public class Ch02_1_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("test");
+		// 자동완성
+		// opt + space 
+		
+		// 행단위 복사
+		// opt + cmd + 방향키
+		
+		// 다중편집
+		// opt + cmd + a
+		// shift로 다중 선택 가능
+		// 단축키 한번 더 눌러서 빠져나옴
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.println();
+		
+	
+		// 사칙연산
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
+		System.out.println(5/3.0);
+		
+		
 	}
 
 }
