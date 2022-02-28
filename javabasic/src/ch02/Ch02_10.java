848b1e4199a88e306efac86d386ba83eddf8b477
heechae96 <shinheechae@naver.com>
2022-02-28 21:12:17 +0900

    기본형과 참조형

---
+++ javabasic/src/ch02/Ch02_10.java
@@ -0,0 +1,24 @@
+ package ch02;
+
+public class Ch02_10 {
+
+	public static void main(String[] args) {
+		
+		// 기본형과 참조형
+		
+		// 기본형
+		// 앞에서 배운 8개
+		// boolean char byte short int long float double
+		// 실제 값을 저장 
+
+		// 참조형
+		// 기본형을 제외한 나머지
+		// 객체 배울때 자세하게 배울 것 
+		// String System 등 무한개
+		// 메모리 주소를 저장 (4바이트 or 8바이트)
+		// 4바이트로 기억하면 좋음
+		// *객체의 주소를 저장한다*
+		
+	}
+
+}
