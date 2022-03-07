93b2b2c997d45216f32f59ea1d05a623749d82ee
heechae96 <shinheechae@naver.com>
2022-03-07 20:38:37 +0900

    2차원 배열

--- javabasic/src/ch05/Ch05_18_20.java
+++ javabasic/src/ch05/Ch05_18_20.java
@@ -3,8 +3,15 @@ package ch05;
 public class Ch05_18_20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// 2차원 배열
+		// 배열의 배열
+		// 테이블 형태의 데이터를 저장하기 위한 배열
+		// int[][] arr = {
+		//					{1,2,3},
+		//					{4,5,6}
+		//							};
+		
 	}
 
 }
