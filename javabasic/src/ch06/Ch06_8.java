ff91a5fd513c5c9d51b607d38cccb0e59849f565
heechae96 <shinheechae@naver.com>
2022-03-08 21:46:58 +0900

    객체 배열

---
+++ javabasic/src/ch06/Ch06_8.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class Ch06_8 {
+
+	public static void main(String[] args) {
+
+		// 객체 배열 == 참조변수 배열
+		// 참조변수의 기본값은 null
+		
+		// 참조변수 배열 생성
+		// Tv[] tvArr = new Tv[3];
+
+		// 객체를 생성해서 배열의 각 요소에 저장
+		// tvArr[0] = new Tv();
+		// tvArr[1] = new Tv();
+		// tvArr[2] = new Tv();
+		
+		// 간단하게
+		// Tv[] tvArr = { new Tv(), new Tv(), new Tv() }
+	}
+
+}
+
