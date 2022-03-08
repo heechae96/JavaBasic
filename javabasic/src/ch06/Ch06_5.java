0acac1725fd7a418a94a440e464592e0e011edd0
heechae96 <shinheechae@naver.com>
2022-03-08 20:45:08 +0900

    한 파일 여러 클래스 작성

---
+++ javabasic/src/ch06/Ch06_5.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class Ch06_5 {
+
+	public static void main(String[] args) {
+
+		// 한 파일 여러 클래스 작성
+		// 1. public class가 있는 경우, 소스파일의 이름은 반드시
+		// public class의 이름과 일치해야함
+		// -> 하나의 소스파일에 public class는 둘 이상이 존재하면 안된다.
+		// 2. public class가 없는 경우, 소스파일의 이름은 둘 다 가능함
+		
+		
+		
+		
+	}
+	
+
+}
+class Hello{}
+class Hello2{}
+
+// Run -> Run Configurations.. -> Main -> main class에서 수동지정 후 실행가능
