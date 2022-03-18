473a9363aa751537de1cd892bf4e84d6a6aa23a5
heechae96 <shinheechae@naver.com>
2022-03-18 15:49:27 +0900

    추상클래스의 작성2

---
+++ javabasic/src/ch07/Ch07_34.java
@@ -0,0 +1,17 @@
+package ch07;
+
+public class Ch07_34 {
+
+	public static void main(String[] args) {
+
+		// 추상화 <-> 구체화
+		// 불명확 <-> 명확
+		
+		// 추상화된 코드는 구체화된 코드보다 유연하다
+		// 	-> 변경에 유리
+		
+		// 추상화는 다형성과 많은 연관!
+		
+	}
+
+}
