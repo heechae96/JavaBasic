1e72041f4c8552f3f574cb3d342be515eae12ee2
heechae96 <shinheechae@naver.com>
2022-03-30 21:43:10 +0900

    Collection, List, Set, Map

---
+++ javabasic/src/ch11/Ch11_3_6.java
@@ -0,0 +1,36 @@
+package ch11;
+
+public class Ch11_3_6 {
+
+	public static void main(String[] args) {
+
+		// Collection
+		//	-> List		Set
+		//	순서/중복o		순서/중복x
+		
+		// Map
+		//	-> 순서x/중복o(키x값o)
+		
+		// 관련 메소드 사진참고!
+		// 추가, 삭제, 검색 등...
+		
+		// List인터페이스 - 순서o, 중복o
+		// Vector, ArrayList, LinkedList
+		//	-> Vector는 old
+		//	-> ArrayList는 new
+		//	ArrayList, LinkedList가 핵심!
+		
+		// Set인터페이스 - 순서x, 중복x
+		// HashSet, SortedSet, TreeSet
+		// HashSet, SortedSet가 핵심
+		
+		// Map인터페이스 - 순서x, 중복(키x, 값o)
+		// Hashtable, HashMap, SortedMap, LinkedHashMap, TreeMap
+		// 	-> Hashtable는 old
+		//	-> HashMap는 new
+		// HashMap, TreeMap이 핵심
+		
+		
+	}
+
+}
